245b4a23ecc19916da26504c1d14940d0b294217
Nandhini <nandhinimu27@gmail.com>
2022-09-14 15:38:23 +0530

    add plan and final component

--- src/components/final.js
+++ src/components/final.js
@@ -1,5 +1,64 @@
-import styles from "../../styles/Home.module.css";
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-export default function Final() {
-  return <div className={styles.container}>Final</div>;
+const theme = createTheme();
+
+export default function Home(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    props.handleComplete();
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <CheckCircleIcon color="primary" sx={{ fontSize: 50, margin: 2 }} />
+          <Typography component="h1" variant="h4">
+            Congratulations, Eren!
+          </Typography>
+          <br />
+          <Typography component="p" variant="h6" sx={{ fontSize: 13 }}>
+            You have completed onboarding, you can start using the Eden!
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Launch Eden
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
--- src/components/planning.js
+++ src/components/planning.js
@@ -1,5 +1,84 @@
-import styles from "../../styles/Home.module.css";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import PersonIcon from "@mui/icons-material/Person";
+import CardContent from "@mui/material/CardContent";
+import Card from "@mui/material/Card";
+import GroupsIcon from "@mui/icons-material/Groups";
 
-export default function Planning() {
-  return <div className={styles.container}>Planning</div>;
+const theme = createTheme();
+
+export default function Planning(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    props.handleComplete();
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h4">
+            How are you planning to use Eden?
+          </Typography>
+          <br />
+          <Typography component="p" variant="p">
+            We`ll streamline your setup experience accordingly.
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <Box sx={{ display: "flex", flexDirection: "row" }}>
+              <Card raised sx={{ maxWidth: 345, marginRight: 2 }}>
+                <CardContent>
+                  <PersonIcon />
+                  <Typography variant="h6" color="text.primary">
+                    For myself
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Write better.Think more clearly.Stay organized
+                  </Typography>
+                </CardContent>
+              </Card>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardContent>
+                  <GroupsIcon />
+                  <Typography variant="h6" color="text.primary">
+                    With my team
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Wikis, docs, tasks & projects, all in one place.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Box>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Create Workspace
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 }
